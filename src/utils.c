b848dbc01e78e5cd86e70702d61a3ff22c07b770
jaypae95 <jaypae95@naver.com>
2017-09-29 19:41:09 +0900

    think im done with utils.c it worked on visual studio

--- src/utils.c
+++ src/utils.c
@@ -1,30 +1,40 @@
 #include "utils.h"
-#include <string.h>
-#include <stdlib.h>
-#include <stio.h>
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+void mysh_parse_command(cont char* command, int *argc, char *** argv)
 {
-  int current = 0;
-  int k = 0;
-  char *token = NULL;
-  char _command[8096] = {'\0', };
+ int current = 0;
+ int k = 0;
+ char * token = NULL;
+ char _command[8096] = {'\0', };
 
-  while(command[current] != NULL) {
-  _command[current] = command[current];
-  current++;
-  }
+ while(command[current] != NULL) {
+   _command[current] = command[current];
+   printf("%c", _command[current]);
+   current++;
+ }
 
-  token = strtok(_command, " ");
-  *argc = 0;
-  char **_argv = (char**)malloc(4*100);
+ token = strtok(_command, " ");
 
-  while (token != NULL) {
-  _argv[*argc] = token;
-  (*argc)++;
-  token = strtok(NULL, " ");
-  }
+ *argc = 0;
+ char **_argv = (char**)malloc(4*100);
 
-  argv = *_argv;
+ while(token!=NULL) {
+   _argv[*argc] = token;
+   (*argc)++;
+   token = strtok(NULL, " ");
+ }
 
-  }
+ *argv = (char**)malloc(sizeof(char));
+
+ for (int i = 0; i < *argc; i++) {
+   argv[0][i] = (char*)malloc(sizeof(char));
+ }
+
+ for (int i = 0; i < *argc; i++) {
+   strcpy(arv[0][i], _argv[i]);
+ }
+
+}
