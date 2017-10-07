3594e75c5277b15e7444166bbb5dde7b048a1e77
jaypae95 <jaypae95@naver.com>
2017-10-08 01:22:40 +0900

    done

--- src/utils.c
+++ src/utils.c
@@ -10,33 +10,40 @@ void mysh_parse_command(const char* command, int *argc, char *** argv)
  char * token = NULL;
  char _command[8096] = {'\0', };
 
+
  while(command[current] != NULL) {
    _command[current] = command[current];
    current++;
  }
 
-  if( _command[strlen(_command)-1] == '\n'){
-   _command[strlen(_command)-1] = '\0';
-   }
+ if( _command[strlen(_command)-1] == '\n'){
+  _command[strlen(_command)-1] = '\0';
+  }
  token = strtok(_command, "' ''\n''\t'");
 
  *argc = 0;
- char **_argv = (char**)malloc(4*100);
-
+ char **_argv = (char**)malloc(sizeof(char)*30);
+ if (token == NULL) {
+ *argc = 1;
+ *argv = (char*)malloc(sizeof(char)*30);
+ argv[0][0] = (char*)malloc(sizeof(char));
+ strcpy(argv[0][0],"");
+ return;
+ }
  while(token!=NULL) {
    _argv[*argc] = token;
    (*argc)++;
    token = strtok(NULL, "' ''\n''\t'");
  }
+ 
 
- *argv = (char**)malloc(sizeof(char));
-
+ *argv = (char**)malloc(sizeof(char)*30);
+ 
  for (int i = 0; i < *argc; i++) {
-   argv[0][i] = (char*)malloc(sizeof(char));
- }
-
+  argv[0][i] = (char*)malloc(sizeof(char)*30);
+ } 
  for (int i = 0; i < *argc; i++) {
-   strcpy(argv[0][i], _argv[i]);
+   strcpy(argv[0][i], _argv[i]); 
  }
 
 }
