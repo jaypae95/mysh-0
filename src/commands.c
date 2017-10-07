48158809a770def90401ad4e6b43de573c6e8941
jaypae95 <jaypae95@naver.com>
2017-10-07 11:44:37 +0900

    first try of getcwd

--- src/commands.c
+++ src/commands.c
@@ -1,5 +1,5 @@
 #include <string.h>
-
+#include <unistd.h>
 #include "commands.h"
 
 int do_cd(int argc, char** argv) {
@@ -14,8 +14,9 @@ int do_pwd(int argc, char** argv) {
   if (!validate_pwd_argv(argc, argv))
     return -1;
 
-  // TODO: Fill it!
-
+  char dirname[200];
+  getcwd(dirname,200);
+  printf("%s", dirname);
   return 0;
 }
 
